64657a177dd43223d5ce3f85d2d9de6e17955681
ebrusoysal <Varan2019>
2019-11-04 15:48:11 +0000

    initial commit

---
+++ src/test/java/com/heyman/RunCucumberTest.java
@@ -0,0 +1,32 @@
+package com.heyman;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "html:target/default-cucumber-reports"
+        },
+
+        tags = {"@sample"},
+
+        features = {"src/test/resources/features"
+        },
+
+        glue = {"com/hymnai/backend/steps"},
+
+        dryRun = false
+
+)
+
+
+
+public class RunCucumberTest {
+
+}
