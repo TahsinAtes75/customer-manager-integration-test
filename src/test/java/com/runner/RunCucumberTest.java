e1a4b06189b73d1315573400295e5edbd3eddeca
Rabia Inan <r.inan@hymnai.com>
2020-08-17 16:56:24 +0100

    Customer Manager - Document Service Update Document

--- src/test/java/com/runner/RunCucumberTest.java
+++ src/test/java/com/runner/RunCucumberTest.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
 
         tags = {},
 
-        features = {"src/test/resources/features/CM_Refactor_Resend_Email_Verification_Link_for_Agent.feature"
+        features = {"src/test/resources/features/CM_Update_document.feature"
         },
 
         glue = {"com/hymnai/backend/steps"},
